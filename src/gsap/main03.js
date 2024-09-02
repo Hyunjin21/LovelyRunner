ca1b5cecdb42cfaba5387cc635aa5668762ce764
Hyunjin <hjin1481@naver.com>
2024-09-02 21:50:27 +0900

    09.02

--- src/gsap/main03.js
+++ src/gsap/main03.js
@@ -4,57 +4,57 @@ const Main03 = () => {
   return (
     <section className='h-dvh' style={{backgroundColor:'#fff', paddingBottom:'300px'}}>
         <div className='tags' style={{background:'linear-gradient(#ffec40, #fff)', height:'100vh', width:'1920px', marginBottom:'21.370vh',paddingTop:'18.518518518519vh'}}>
-          <div className='leading-none' style={{paddingLeft: '1.465vw',paddingRight: '1.465vw'}}>
+          <div className='leading-none' style={{position:'relative',paddingLeft: '1.465vw',paddingRight: '1.465vw'}}>
             <div className='title-wrap'>
               <h2 style={{fontSize:'13.542vw', fontWeight:500, display:'block'}}>Thank you 
                 <br/>for being alive</h2> 
               {/* <span ref={textRef} className='w-max whitespace-nowrap block' style={{fontSize:'18.750vw', fontWeight: 700, color:'#86bee7'}}>Lovely Runner</span> */}
             </div>
-            <div className='tags-wrap'>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>Eclipse</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>HanRiver</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>Wristwatch</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>12AM</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>Video</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'3.125vw', color:'#fff', padding:'0.521vw 2.969vw'}}>Swimming</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'3.125vw', color:'#fff', padding:'0.521vw 2.969vw'}}>TimeCapsule</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'3.125vw', color:'#fff', padding:'0.521vw 2.969vw'}}>Necklace</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>AmusementPark</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>Wheelchair</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>Snow</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'3.125vw', color:'#fff', padding:'0.521vw 2.969vw'}}>Umbrella</span>
-              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'3.125vw', padding:'0.521vw 2.969vw'}}>MP3player</span>
+            <div className='tags-wrap' style={{position:'absolute',top:'50%',left:'50%',width:'100%',height:'100%',maxWidth:'1920px',margin:'0 auto',transform:'translate(-50%,-50%)'}}>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(7.00001deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', top:'3%',left:'7%'}}>Eclipse</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(-27.5298deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', top:'23%',left:'11.75%'}}>HanRiver</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(15.6298deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', top:'-2%',left:'35.8%'}}>Wristwatch</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(-12.13deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', top:'18%',left:'47.5%'}}>12AM</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(-28.93deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', top:'1.5%',right:'12%'}}>Video</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(19.83deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'2.225vw', color:'#fff', padding:'0.521vw 2.969vw', position:'absolute', top:'38%',left:'26.8%'}}>Swimming</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(8.80022deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'2.225vw', color:'#fff', padding:'0.521vw 2.969vw', position:'absolute', top:'36.5%',right:'16%'}}>TimeCapsule</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(33.1401deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'2.225vw', color:'#fff', padding:'0.521vw 2.969vw', position:'absolute', bottom:'4%',left:'6.5%'}}>Necklace</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(-14.03deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', bottom:'20%',left:'18%'}}>Snow</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(8.80022deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', bottom:'15%',left:'38%'}}>Wheelchair</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(13.7703deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', bottom:'-18%',left:'44%'}}>AmusementPark</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(-13.1099deg) scale(1.5, 1.5)', backgroundColor:'#86bee7', borderRadius:'3.604vw', fontSize:'2.225vw', color:'#fff', padding:'0.521vw 2.969vw', position:'absolute', bottom:'0%',right:'28.8%'}}>Umbrella</span>
+              <span style={{opacity:1, visibility:'inherit', transform:'rotate(23.0498deg) scale(1.5, 1.5)', backgroundColor:'#fff', borderRadius:'3.604vw', fontSize:'2.225vw', padding:'0.521vw 2.969vw', position:'absolute', bottom:'9%',right:'8%'}}>MP3player</span>
 
             </div>
           </div>
         </div>
         <div className='scroll flex items-start flex-row' style={{background:'#fff'}}>
-          <div className='part1'>
+          <div className='part1 flex justify-center items-center flex-col h-dvh w-full' style={{width:'1920px'}}>
             <div className='img_wrap'>
             </div>
-            <div className='ost_part' style={{color:'#0cd886',fontSize:'34.375vw', fontFamily: "Oswald, sans-serif"}}>
-              <span>PART1</span>
+            <div className='ost_part flex justify-center items-center m-0' style={{width:'1920px'}}>
+              <span style={{color:'#0cd886',fontSize:'34.375vw', fontFamily: "Oswald, sans-serif", fontWeight:700, letterSpacing:1.5}}>PART1</span>
             </div>
-            <div className='ost_info'>
-              <div className='ost_title'>
+            <div className='ost_info flex justify-center items-center flex-col' style={{marginTop:'-9.635vw', width:'100%', marginLeft:'auto',marginRight:'auto', paddingLeft:'3.646vw',paddingRight:'3.646vw'}}>
+              <div className='ost_title' style={{fontSize:'4.167vw', lineHeight:'1.04', fontWeight:500}}>
                 <span>Rain Shower</span>
               </div>
-              <div className='ost_singer'>
+              <div className='ost_singer' style={{fontSize:'18px', lineHeight:'1.78', marginTop: '10px'}}>
                 <span>Eclipse</span>
               </div>
             </div>
           </div>
-          <div className='part2'>
+          <div className='part2 flex justify-center items-center flex-col h-dvh w-full' style={{width:'1920px', paddingLeft:'3.646vw',paddingRight:'3.646vw'}}>
             <div className='img_wrap'>
             </div>
-            <div className='ost_part' style={{color:'#86bee7',fontSize:'34.375vw', fontFamily: "Oswald, sans-serif"}}>
-              <span>PART2</span>
+            <div className='ost_part m-0'>
+              <span style={{color:'#86bee7',fontSize:'34.375vw', fontFamily: "Oswald, sans-serif", fontWeight:700}}>PART2</span>
             </div>
-            <div className='ost_info'>
-              <div className='ost_title'>
+            <div className='ost_info flex justify-center items-center flex-col' style={{marginTop:'-9.635vw', width:'100%', marginLeft:'auto',marginRight:'auto'}}>
+              <div className='ost_title' style={{fontSize:'4.167vw', lineHeight:'1.04', fontWeight:500}}>
                 <span>Star</span>
               </div>
-              <div className='ost_singer'>
+              <div className='ost_singer' style={{fontSize:'18px', lineHeight:'1.78', marginTop: '10px'}}>
                 <span>N.Flying</span>
               </div>
             </div>
