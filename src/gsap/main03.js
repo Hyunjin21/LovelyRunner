35b145a372bae13cb1698b2c36b411a1f6ec009a
Hyunjin <hjin1481@naver.com>
2024-09-02 19:06:28 +0900

    09.02

--- src/gsap/main03.js
+++ src/gsap/main03.js
@@ -2,10 +2,105 @@ import React from 'react'
 
 const Main03 = () => {
   return (
-    <section ref={triggerRef} className='h-dvh flex justify-center items-start' style={{backgroundColor:'#ffec40'}}>
-        <div ref={titleRef} className='uppercase leading-none'>
-          
-          <span ref={textRef} className='w-max whitespace-nowrap block' style={{fontSize:'18.750vw', fontWeight: 700, color:'#86bee7'}}>Lovely Runner</span>
+    <section className='h-dvh flex justify-center items-start flex-col' style={{backgroundColor:'#ffec40'}}>
+        <div className='tags'>
+          <div className='leading-none'>
+            <div className='title-wrap'>
+              <h2 style={{fontSize:'13.542vw', fontWeight:700}}>Thank you 
+                <br/>For being alive</h2> 
+              {/* <span ref={textRef} className='w-max whitespace-nowrap block' style={{fontSize:'18.750vw', fontWeight: 700, color:'#86bee7'}}>Lovely Runner</span> */}
+            </div>
+            <div className='tags-wrap'>
+              <span>Eclipse</span>
+              <span>HanRiver</span>
+              <span>Wristwatch</span>
+              <span>12AM</span>
+              <span>Video</span>
+              <span>Swimming</span>
+              <span>TimeCapsule</span>
+              <span>Necklace</span>
+              <span>AmusementPark</span>
+              <span>Wheelchair</span>
+
+            </div>
+          </div>
+        </div>
+        <div className='scroll flex items-start flex-row'>
+          <div className='part1'>
+            <div className='img_wrap'>
+            </div>
+            <div className='ost_part' style={{fontSize:'34.375vw'}}>
+              <span>PART1</span>
+            </div>
+            <div className='ost_info'>
+              <div className='ost_title'>
+                <span>Rain Shower</span>
+              </div>
+              <div className='ost_singer'>
+                <span>Eclipse</span>
+              </div>
+            </div>
+          </div>
+          <div className='part2'>
+            <div className='img_wrap'>
+            </div>
+            <div className='ost_part' style={{fontSize:'34.375vw'}}>
+              <span>PART2</span>
+            </div>
+            <div className='ost_info'>
+              <div className='ost_title'>
+                <span>Star</span>
+              </div>
+              <div className='ost_singer'>
+                <span>N.Flying</span>
+              </div>
+            </div>
+          </div>
+          <div className='part3'>
+            <div className='img_wrap'>
+            </div>
+            <div className='ost_part' style={{fontSize:'34.375vw'}}>
+              <span>PART3</span>
+            </div>
+            <div className='ost_info'>
+              <div className='ost_title'>
+                <span>It's like a dream</span>
+              </div>
+              <div className='ost_singer'>
+                <span>Minnie (G)I-DLE</span>
+              </div>
+            </div>
+          </div>
+          <div className='part4'>
+            <div className='img_wrap'>
+            </div>
+            <div className='ost_part' style={{fontSize:'34.375vw'}}>
+              <span>PART8</span>
+            </div>
+            <div className='ost_info'>
+              <div className='ost_title'>
+                <span>Spring Snow</span>
+              </div>
+              <div className='ost_singer'>
+                <span>10CM</span>
+              </div>
+            </div>
+          </div>
+          <div className='part5'>
+            <div className='img_wrap'>
+            </div>
+            <div className='ost_part' style={{fontSize:'34.375vw'}}>
+              <span>PART9</span>
+            </div>
+            <div className='ost_info'>
+              <div className='ost_title'>
+                <span>Present</span>
+              </div>
+              <div className='ost_singer'>
+                <span>Ha Sung Woon</span>
+              </div>
+            </div>
+          </div>
         </div>
     </section>   
   )
